de23a85f6209e0c58a529da07b86f9888695224c
Castor Köhler <accounts@castorkohler.com>
2024-03-26 17:30:53 +0200

    Add basic form and table of books

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 
 interface Book {
   id: number;
@@ -9,7 +8,7 @@ interface Book {
 }
 
 const TEST_DATA: Book[] = [
-  {id:1, title: "First", author: "Stoneman", description: "Im the first book ever made!"},
+  {id:1, title: "First", author: "Stoneman", description: "I'm the first book ever made!"},
   {id:2, title: "Second", author: "Bronzeman", description: "Ironically, the second book written by BRONZEman"},
   {id:3, title: "Third", author: "Ironman", description: "TOO DOO TOO DOO DOO TODODODODO DOO DOO DO DO"},
   {id:4, title: "Fourth", author: "Mr. Fantastic", description: "Four is a good number"},
@@ -18,27 +17,39 @@ const TEST_DATA: Book[] = [
 
 function App() {
   const [books, setBooks] = useState<Book[]>(TEST_DATA);
+  const [activeBook, setActiveBook] = useState<Book | null>(null);
 
   return (
-    <>
-      <h1>BOOKS</h1>
-      <table>
-        <thead>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Description</th>
-        </thead>
-        <tbody>
-          {books.map(book => (
-            <tr>
-              <td>{book.title}</td>
-              <td>{book.author}</td>
-              <td>{book.description}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+    <div className='flex flex-col items-center'>
+      <h1 className='text-5xl font-bold py-20'>Welcome to Castor's library!</h1>
+      <div className='flex w-min justify-center border-2 rounded-lg'>
+        <div className='basis-1 border-r-2 p-12'>
+          <table>
+            <tbody>
+              {books.map(book => (
+                <tr onClick={() => setActiveBook(book)}>
+                  <td>{book.title}</td>
+                  <td>{book.author}</td>
+                  <td>{book.description}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className='basis-1 p-12'>
+          <form>
+            <label htmlFor="title" className='block'>Title</label>
+            <input className="border-2" id="title" name="title" type="text" placeholder='Title of the book' value={activeBook?.title}></input>
+
+            <label htmlFor="author" className='block'>Author</label>
+            <input className="border-2" id="author" name="author" type="text" placeholder='Author of the book' value={activeBook?.author}></input>
+
+            <label htmlFor="description" className='block'>Description</label>
+            <textarea className="border-2" id="description" rows={4} cols={50} name="description" value={activeBook?.description} placeholder='Description of the book'></textarea>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 

6d71b30ae44b22ce83b08a07a68354a1e8eef455
Castor Köhler <accounts@castorkohler.com>
2024-03-26 16:44:01 +0200

    Add basic site with list of books

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+  description: string;
+}
+
+const TEST_DATA: Book[] = [
+  {id:1, title: "First", author: "Stoneman", description: "Im the first book ever made!"},
+  {id:2, title: "Second", author: "Bronzeman", description: "Ironically, the second book written by BRONZEman"},
+  {id:3, title: "Third", author: "Ironman", description: "TOO DOO TOO DOO DOO TODODODODO DOO DOO DO DO"},
+  {id:4, title: "Fourth", author: "Mr. Fantastic", description: "Four is a good number"},
+  {id:5, title: "Mumbo no 5", author: "Lou Bega", description: "A little bit of many women"},
+]
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [books, setBooks] = useState<Book[]>(TEST_DATA);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>BOOKS</h1>
+      <table>
+        <thead>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Description</th>
+        </thead>
+        <tbody>
+          {books.map(book => (
+            <tr>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td>{book.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
